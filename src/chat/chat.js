3771de477f02f243703ef63ad2dccd226e094149
Fredrik Oseberg <fredrik.no@gmail.com>
2021-02-08 23:48:16 +0100

    feat: remove ci

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -89,12 +89,11 @@ const createForm = (messageParserInstance, updater) => {
 
   form.onsubmit = handleSubmit;
 
-  const img = document.createElement("img");
-  img.src = "/assets/icons/paper-plane.svg";
-  img.alt = "paper plane";
-  img.classList.add("vanilla-chatbot-kit-chat-btn-send-icon");
+  const imgContainer = document.createElement("div");
+  imgContainer.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#fff" d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z"/></svg>`;
+  imgContainer.classList.add("vanilla-chatbot-kit-chat-btn-send-icon");
 
-  btn.appendChild(img);
+  btn.appendChild(imgContainer);
   form.appendChild(input);
   form.appendChild(btn);
   container.appendChild(form);
@@ -113,10 +112,9 @@ const createUserChatMessage = (message) => {
     "vanilla-chatbot-kit-user-avatar-container"
   );
 
-  const img = document.createElement("img");
-  img.classList.add("vanilla-chatbot-kit-user-avatar-icon");
-  img.src = "/assets/icons/user-alt.svg";
-  img.alt = "user";
+  const imgContainer = document.createElement("div");
+  imgContainer.classList.add("vanilla-chatbot-kit-user-avatar-icon");
+  imgContainer.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#fff" d="M256 288c79.5 0 144-64.5 144-144S335.5 0 256 0 112 64.5 112 144s64.5 144 144 144zm128 32h-55.1c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16H128C57.3 320 0 377.3 0 448v16c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48v-16c0-70.7-57.3-128-128-128z"/></svg>`;
 
   const messageContainer = document.createElement("div");
   messageContainer.classList.add("vanilla-chatbot-kit-user-chat-message");
@@ -126,7 +124,7 @@ const createUserChatMessage = (message) => {
   arrow.classList.add("vanilla-chatbot-kit-user-chat-message-arrow");
 
   messageContainer.appendChild(arrow);
-  avatarContainer.appendChild(img);
+  avatarContainer.appendChild(imgContainer);
 
   container.appendChild(messageContainer);
   container.appendChild(avatarContainer);
@@ -141,7 +139,6 @@ export const createBotChatMessage = (
   updater
 ) => {
   const { message, widget, loading, id } = mes;
-  console.log(mes);
 
   const outerContainer = document.createElement("div");
 
